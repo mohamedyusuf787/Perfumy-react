9b623ab8c12013d1a95eae86813d448bd2872b59
mohamedyusuf787 <mohamedyusuf13m2003@gmail.com>
2025-12-25 15:57:10 +0530

    npm installed

--- src/index.js
+++ src/index.js
@@ -1,5 +1,9 @@
 import DOM from "react-dom/client";
+<<<<<<< HEAD
 import App from "App.js"
+=======
+import App from "App.js";
+>>>>>>> 4ecc09c (npm installed)
 
 
 const root=DOM.createRoot(document.getElementById("root"))
