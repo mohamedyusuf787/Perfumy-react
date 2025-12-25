8fd3bd5d2c0949b64f2b4e59dcd266004db9b3d7
mohamedyusuf787 <mohamedyusuf13m2003@gmail.com>
2025-12-25 17:36:21 +0530

    updated

--- src/index.js
+++ src/index.js
@@ -1,10 +1,5 @@
 import DOM from "react-dom/client";
-<<<<<<< HEAD
-import App from "App.js"
-=======
-import App from "App.js";
->>>>>>> 4ecc09c (npm installed)
-
+import App from "./App.js";
 
 const root=DOM.createRoot(document.getElementById("root"))
 
@@ -12,6 +7,4 @@ const root=DOM.createRoot(document.getElementById("root"))
 
 
 
-root.render(<App></App>
-  
-)
+root.render(<App></App>)
